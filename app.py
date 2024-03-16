fd0421eb70151977211170101aae5e4669fd3112
gruand <gruand@gmail.com>
2024-03-16 14:40:11 +0200

    Get data when submit, Prepared sqlite db with SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+my_app_secret_key = os.environ["FLASK_APP_1_SECRET_KEY"] # Get from my environment
+
+app.config["SECRET_KEY"] = my_app_secret_key
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
 
 
-@app.route("/")
+# Specify sqlite db structure
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
+
+
+
+
+
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]  # Name of the input in html
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+
     return render_template("index.html")
 
 
-app.run(debug=True)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True)
+
