ae45d6ef781b691a52b2118424c72ca6d9a3992f
gruand <gruand@gmail.com>
2024-03-18 15:16:38 +0200

    Send submission details to email with the help of flask_mail

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 from datetime import datetime
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
 my_app_secret_key = os.environ["FLASK_APP_1_SECRET_KEY"]  # Get from my environment for SQLAlchemy
+MAIL_PASS = os.environ["GMAIL_APP_GRUANDAPP_KEY"] # Get my gmail app pass from environment
 
 app.config["SECRET_KEY"] = my_app_secret_key
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+
+# Add gmail sending parameters
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "gruandapp@gmail.com"
+app.config["MAIL_PASSWORD"] = MAIL_PASS
+
 db = SQLAlchemy(app)
 
+mail = Mail(app)
 
 class Form(db.Model):
     # Specify sqlite db structure
@@ -38,6 +49,17 @@ def index():
         db.session.add(form)
         db.session.commit()
 
+        message_body = (f"Dear {first_name} {last_name}."
+                        f"Thank you for your submission!\n"
+                        f"We've received your data:\n"
+                        f"{first_name}\n{last_name}\n{date}\n{occupation}")
+
+        message = Message(subject="Flask job application form submission",
+                          sender=app.config["MAIL_USERNAME"],
+                          recipients=[email, "gruand+333@gmail.com"],
+                          body=message_body)
+
+        mail.send(message)
         # Success message (jinja 2 in html)
         flash(f"Thank you {first_name}! Your form was submitted successfully!", "success")
 
